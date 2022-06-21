960dffcbdc4407fdc72d8cb6dd2fc310c47c6e90
anuraga091 <anuraga091@gmail.com>
2022-06-21 13:12:02 +0530

    Initial

---
+++ src/Data Constants/Data.jsx
@@ -0,0 +1,20 @@
+ export const imageData = [
+    {
+        url: './images/rage coffee img.png',
+        alt: 'Rage Coffee',
+        name: 'Rage Coffee',
+        story: 'Started in 2018, Rage Coffee is the world’s first plant-based vitamins coffee brand. Bringing the craft coffee experience to the instant and health conscious generation, our coffee is handpicked from best plantations in Ethiopia  and India.'
+    }
+    // {
+    //     url: './images/Polka Pops img.png',
+    //     alt: 'Polka Pop',
+    //     name: 'Polka Pop',
+    //     story: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sodales turpis magna, at dignissim turpis convallis at. Aliquam lacus leo, tristique mattis luctus egestas, condimentum eget ligula.'
+    // },
+    // {
+    //     url: './images/tbc img.png',
+    //     alt: 'The Butternut Co.',
+    //     name: 'The Butternut Co.',
+    //     story: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sodales turpis magna, at dignissim turpis convallis at. Aliquam lacus leo, tristique mattis luctus egestas, condimentum eget ligula.'
+    // }
+]
---
+++ src/Home/Home.jsx
@@ -0,0 +1,177 @@
+import React from 'react';
+import { imageData } from '../Data Constants/Data';
+import { styled, Button } from '@mui/material';
+
+const Home = () => {
+  return (
+    <div style={{backgroundColor: '#171717'}}>
+        <StylePElement>Spotlight</StylePElement>
+        
+        { imageData.map(data => (
+          <StyleDivElement>
+            <div className='img'>
+              <img src="./images/spotlight logo.jpg" alt="Spotlight Logo"/> <span> X </span><img src={data.url} alt={data.alt}/>
+            </div>
+            <h6>
+              <b>{data.name}</b>
+              <span> is now on </span>
+              <b> Spotlight </b>
+            </h6>
+            <hr/>
+            <div className='card'>
+                <p>Visit the nearest store for <br/> <i> exclusive deals </i> </p>
+                <Button><img src="./images/map_pin.png" alt="icon"/> Find a Store Near Me</Button>
+            </div>
+
+            <div className="story">
+              <div className='heading'>
+                <img className='image1' src={data.url} alt={data.alt}/>
+                <div>
+                  <h5>STORY OF</h5> 
+                  <p>{data.name}</p>
+                </div>
+              </div>
+              <div className='body'>
+              <img className='image2' src={data.url} alt=""/>
+              <p>{data.story}</p>
+              </div>
+            </div>
+
+            <div>
+              <img src="" alt=""/>
+              <p></p>
+              <img src="" alt=""/>
+              <p></p>
+              <img src="" alt=""/>
+              <p></p>
+            </div>
+          </StyleDivElement>
+        ))}
+    </div>
+  )
+};
+
+const StylePElement = styled('p')`
+  padding: 10px 20px;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 24px;
+  background-color: black;
+  width: auto;
+`;
+
+const StyleDivElement = styled('div')`
+  
+  .img{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
+    }
+
+  img{
+    width: 112px;
+    height: 112px;
+    
+    border-radius: 50%;
+  }
+  span{
+    margin-top: 50px;
+  }
+  h6{
+    text-align: center;
+    font-size: 18px;
+    line-height: 24px;
+    margin: 10px;
+  }
+
+  hr{
+    width: 218px;
+    height: 0px;
+    margin: 2px 0 0 20%;
+    background: rgba(217, 217, 217, 0.6);
+    opacity: 0.2;
+    border: 1px solid #ADADAD;
+  }
+  
+
+  .card{
+    margin: 10px 20px;
+    background: linear-gradient(180deg, rgba(58, 58, 58, 0.7) 5.22%, rgba(72, 72, 72, 0.4) 94.94%);
+    backdrop-filter: blur(12px);
+    /* Note: backdrop-filter has minimal browser support */
+    border-radius: 5px;
+
+    p{
+    text-align: center;
+    margin: 10px 0px 5px 0px;
+    i{
+      font-weight: 700;
+      color: #F38137;
+    }
+    }
+    button{
+      background: linear-gradient(90deg, #3F0BDB 0%, #FF0C67 100%);
+      border-radius: 5px;
+      margin: 10px 20px 30px 20px;
+      font-weight: 600;
+      font-size: 16px;
+      text-transform: none;
+      color: #fff;
+
+      img{
+        width: 24px;
+        height: 24px;
+        margin-right: 5px;
+      }
+    }
+  }
+
+  .story{
+     background: linear-gradient(180deg, rgba(58, 58, 58, 0.7) 5.22%, rgba(72, 72, 72, 0.4) 94.94%);
+     backdrop-filter: blur(12px);
+     border-radius: 5px;
+     margin:  20px;
+
+    .heading{
+      display: flex;
+      color: rgba(255, 255, 255, 0.6);
+      padding: 10px 20px;
+      .image1{
+        width: 50px;
+        height: 50px;
+        border-radius: 2px;
+        margin-right: 10px;
+       
+      }
+      h5{
+        margin-bottom: 0;
+        font-size: 24px;
+      }
+      p{
+        font-size: 14px;
+        text-transform: uppercase;
+        margin-bottom: 0;
+      }
+    }
+    
+    .body{
+      display: flex;
+      justify-content: space-evenly;
+      margin-top: 20px;
+      .image2{
+        width: 150px;
+        height: 150px;
+        border-radius: 0;
+        margin: 10px ;
+      }
+      p{
+        font-size: 14px;
+        color: rgba(255, 255, 255, 0.6);
+        margin-right: 5px;
+      }
+    }
+    
+  }
+`;
+
+export default Home;
