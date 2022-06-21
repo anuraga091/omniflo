b03142be769c84ebcb792c158eaa056000c1f8a0
anuraga091 <anuraga091@gmail.com>
2022-06-21 17:08:39 +0530

    Structure complete

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { imageData } from '../Data Constants/Data';
 import { styled, Button } from '@mui/material';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Home = () => {
   return (
@@ -48,6 +50,20 @@ const Home = () => {
                 </div> 
               </div>
             </div>
+
+            <footer>
+              <div className='footer'>
+                <img src="./images/spotlight logo.jpg" alt="spotlight logo"/>
+                <p>Spotlight is The Coolest Retail Shelf that brings premium online brands near you!</p>
+              </div>
+              <div style={{display: 'flex', justifyContent: 'center'}} className="MUIIcon">
+                <InstagramIcon />
+                <TwitterIcon/>
+              </div>
+              
+              <hr/>
+              <p> &copy; 2022 Spotlight</p>
+            </footer>
             
           </StyleDivElement>
         ))}
@@ -134,6 +150,7 @@ const StyleDivElement = styled('div')`
      backdrop-filter: blur(12px);
      border-radius: 5px;
      margin:  20px;
+     padding: 20px 0px;
 
     .heading{
       display: flex;
@@ -205,6 +222,43 @@ const StyleDivElement = styled('div')`
 
     }
     
+  }
+
+  footer{
+    padding: 20px 40px;
+    background-color: rgba(43, 43, 43, 1);
+    .footer{
+      display: flex;
+      margin-bottom: 10px;
+      img{
+        width: 30px;
+        height: 30px;
+        margin-right: 10px;
+      }
+
+      p{
+        color: rgba(173, 173, 173, 1);
+        font-size: 11px;
+        text-align: unset;
+        margin: 0;
+      }
+    }
+    hr{
+      border: 0.8px solid #383838;
+    }
+    p{
+      font-size: 11px;
+      color: rgba(153, 153, 153, 1);
+      margin: 5px;
+      text-align: center;
+    }
+    svg{
+      font-size: 20px;
+      text-align: center;
+      align-items: center;
+      margin: 10px;
+      color: rgba(153, 153, 153, 1);
+    }
   }
 `;
 
