72fbf04cab3271e8002e0ecb3786aed1a2d2658b
anuraga091 <anuraga091@gmail.com>
2022-06-22 15:28:47 +0530

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Home from './Home/Home';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import RageCoffee from './Brand Landing Page/RageCoffee';
 
 function App() {
   return (
     <div>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/rageCoffee" element={<RageCoffee/>}></Route>
+          {/* <Route path="/home" element={<><Header /><Home/></>}></Route> */}
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
