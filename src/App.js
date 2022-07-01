f6b53db69bbbf2faf3bbe5647048a5799f7609fc
anuraga091 <anuraga091@gmail.com>
2022-07-01 16:21:10 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import RageCoffee from './Brand Landing Page/RageCoffee';
 import RageCoffeeStores from './Stores/RageCoffeeStores';
 import LocationDenyPage from './Brand Landing Page/LocationDenyPage';
 import StoreNotFound from './Brand Landing Page/StoreNotFound';
+import LoadingScreen from './loader/LoadingScreen';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/rageCoffee/stores" element={<RageCoffeeStores/>}></Route>
           <Route path="/rageCoffee/locationdenypage" element={<LocationDenyPage/>}></Route>
           <Route path="/rageCoffee/storenotfound" element={<StoreNotFound/>}></Route>
+          <Route path="/loading" element={<LoadingScreen/>}></Route>
           {/* <Route path="/home" element={<><Header /><Home/></>}></Route> */}
           
         </Routes>
