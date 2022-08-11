f8031ff93a1217fcaf8f61710913233d5821ee18
anuraga091 <anuraga091@gmail.com>
2022-08-11 12:20:40 +0530

    updated image alignment

--- src/components/Usp.js
+++ src/components/Usp.js
@@ -80,7 +80,7 @@ const StyleDivElement = styled('div')`
       margin: 20px;
       
       .image2{
-        width: 80%;
+        width: 100%;
         height: 200px;
         border-radius: 0;
         margin-bottom: 10px;
